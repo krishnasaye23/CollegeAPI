ea223c084cdbc6082e73111a6f74773c448d3024
Krishna A S <krishnaas@192.168.0.2>
2021-06-04 18:29:30 +0530

    https://github.com/krishnasaye23/CollegeAPI/issues/5#issue-910218269

--- src/main/java/com/example/schoolapi/Repository/ClubRepo.java
+++ src/main/java/com/example/schoolapi/Repository/ClubRepo.java
@@ -1,2 +1,13 @@
-package com.example.schoolapi.Repository;public interface ClubRepo {
+package com.example.schoolapi.Repository;
+
+import com.example.schoolapi.Model.Clubs;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ClubRepo extends JpaRepository<Clubs,Integer> {
+    List<Clubs> findAll();
+    Clubs deleteById(int club_id);
+    Clubs findById(int club_id);
 }
--- src/main/java/com/example/schoolapi/Repository/DepartmentRepo.java
+++ src/main/java/com/example/schoolapi/Repository/DepartmentRepo.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface DepartmentRepo extends JpaRepository<Department,String> {
+public interface DepartmentRepo extends JpaRepository<Department,Integer> {
     List<Department> findAll();
     Department deleteById(int dept_id);
     Department findById(int dept_id);
