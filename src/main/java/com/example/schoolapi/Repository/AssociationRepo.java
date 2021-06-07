d9685d7665be1b1aec289a1c7d9a760f46beb5d2
Krishna A S <krishnaas@192.168.0.2>
2021-06-07 18:53:19 +0530

    https://github.com/krishnasaye23/CollegeAPI/issues/5#issue-910218269

--- src/main/java/com/example/schoolapi/Repository/AssociationRepo.java
+++ src/main/java/com/example/schoolapi/Repository/AssociationRepo.java
@@ -1,2 +1,14 @@
-package com.example.schoolapi.Repository;public class AssociationRepo {
+package com.example.schoolapi.Repository;
+
+import com.example.schoolapi.Model.Dept_Association;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AssociationRepo extends JpaRepository<Dept_Association,Integer> {
+    List<Dept_Association> findAll();
+    Dept_Association deleteById(int asc_id);
+    Dept_Association findById(int asc_id);
 }
