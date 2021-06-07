9cda433b503ae912340892f788ba43a05efa1ec9
Krishna A S <krishnaas@192.168.0.2>
2021-06-07 18:50:38 +0530

    https://github.com/krishnasaye23/CollegeAPI/issues/4#issue-910215271

--- src/main/java/com/example/schoolapi/Service/AssociationsServ.java
+++ src/main/java/com/example/schoolapi/Service/AssociationsServ.java
@@ -1,2 +1,13 @@
-package com.example.schoolapi.Service;public class AssociationsServ {
+package com.example.schoolapi.Service;
+
+import com.example.schoolapi.Model.Dept_Association;
+
+import java.util.List;
+
+public interface AssociationsServ {
+    List<Dept_Association> getAssociation();
+    Dept_Association getAssociationbyid(int asc_id);
+    Dept_Association addAssociation(Dept_Association dept_association);
+    Dept_Association updateAssociation(Dept_Association dept_association);
+    Dept_Association delrecord(int asc_id);
 }
--- src/main/java/com/example/schoolapi/Service/AssociationsServImpl.java
+++ src/main/java/com/example/schoolapi/Service/AssociationsServImpl.java
@@ -1,2 +1,40 @@
-package com.example.schoolapi.Service;public class AssociationsServImpl {
+package com.example.schoolapi.Service;
+
+import com.example.schoolapi.Model.Dept_Association;
+import com.example.schoolapi.Repository.AssociationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Component
+public class AssociationsServImpl implements AssociationsServ{
+    @Autowired
+    private AssociationRepo associationRepo;
+    @Override
+    public List<Dept_Association> getAssociation() {
+        return associationRepo.findAll();
+    }
+    @Override
+    public Dept_Association getAssociationbyid(int asc_id) {
+        Dept_Association opt=associationRepo.findById(asc_id);
+        return opt;
+    }
+    @Override
+    public Dept_Association addAssociation(Dept_Association dept_association){
+        associationRepo.save(dept_association);
+        return dept_association;
+    }
+    @Override
+    public Dept_Association updateAssociation(Dept_Association dept_association){
+        associationRepo.save(dept_association);
+        return dept_association;
+    }
+    @Override
+    public Dept_Association delrecord(int asc_id) {
+        Dept_Association g=associationRepo.deleteById(asc_id);
+        return g;
+    }
 }
