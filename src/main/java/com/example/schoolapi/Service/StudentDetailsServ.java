42f2bff56c9f9d8234a3663f42d2901facd39385
Krishna A S <krishnaas@192.168.0.2>
2021-06-08 16:09:30 +0530

    https://github.com/krishnasaye23/CollegeAPI/issues/4#issue-910215271

---
+++ src/main/java/com/example/schoolapi/Service/StudentDetailsServ.java
@@ -0,0 +1,12 @@
+package com.example.schoolapi.Service;
+
+import com.example.schoolapi.Model.StudentsDetails;
+
+import java.util.List;
+
+public interface StudentDetailsServ {
+    List<StudentsDetails> getStudentDetails();
+    StudentsDetails getStudentDetailsbyid(int student_id);
+    StudentsDetails addStudentDetails(StudentsDetails studentsDetails);
+    StudentsDetails updateStudentDetails(StudentsDetails studentsDetails);
+}
---
+++ src/main/java/com/example/schoolapi/Service/StudentDetailsServImpl.java
@@ -0,0 +1,35 @@
+package com.example.schoolapi.Service;
+
+import com.example.schoolapi.Model.StudentsDetails;
+import com.example.schoolapi.Repository.StudentDetailsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Component
+@Service
+public class StudentDetailsServImpl implements StudentDetailsServ {
+    @Autowired
+    private StudentDetailsRepo studentDetailsRepo;
+    @Override
+    public List<StudentsDetails> getStudentDetails() {
+        return studentDetailsRepo.findAll();
+    }
+    @Override
+    public StudentsDetails getStudentDetailsbyid(int student_id) {
+        StudentsDetails opt=studentDetailsRepo.findById(student_id);
+        return opt;
+    }
+    @Override
+    public StudentsDetails addStudentDetails(StudentsDetails studentsDetails){
+        studentDetailsRepo.save(studentsDetails);
+        return studentsDetails;
+    }
+    @Override
+    public StudentsDetails updateStudentDetails(StudentsDetails studentsDetails){
+        studentDetailsRepo.save(studentsDetails);
+        return studentsDetails;
+    }
+}
