bdca2202f76b443496448aff1530a2f2bd5c80a0
Krishna A S <krishnaas@192.168.0.2>
2021-06-08 17:54:20 +0530

    Resources commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ CREATE TABLE if not exists department
     dept_id integer NOT NULL PRIMARY KEY,
     dept_name varchar(25) NOT NULL,
     hod varchar(25),
-    FOREIGN KEY (Student_id) REFERENCES Students(student_id),
+    FOREIGN KEY (Student_id) REFERENCES students(student_id),
     FOREIGN KEY (Teacher_id) REFERENCES Teachers(teacher_id),
     FOREIGN KEY (Asc_id) REFERENCES Dept_Association(asc_id)
 );
@@ -14,7 +14,7 @@ CREATE TABLE if not exists clubs
     president varchar(25),
     vice_president varchar(25),
     about varchar(100),
-    FOREIGN KEY (Student_id) REFERENCES Students(student_id)
+    FOREIGN KEY (Student_id) REFERENCES students(student_id)
     );
 CREATE TABLE if not exists dept_association
 (
@@ -24,9 +24,9 @@ CREATE TABLE if not exists dept_association
     vice_president varchar(25),
     Dept_id integer not null,
     FOREIGN KEY (Dept_id) REFERENCES Department(dept_id),
-    FOREIGN KEY (Student_id) REFERENCES Students(student_id)
+    FOREIGN KEY (Student_id) REFERENCES students(student_id)
     );
-CREATE TABLE if not exists Students
+CREATE TABLE if not exists students
 (
     student_id integer NOT NULL,
     student_name varchar(25),
@@ -41,6 +41,17 @@ CREATE TABLE if not exists Students
     FOREIGN KEY (Dept_id) REFERENCES department(dept_id),
     FOREIGN KEY (Club_id) REFERENCES department(club_id),
     FOREIGN KEY (Asc_id) REFERENCES department(asc_id)
+);
+CREATE TABLE IF NOT EXISTS students_details
+(
+    id integer,
+    father_name varchar(20),
+    mother_name varchar(20),
+    email varchar(25),
+    cgpa double,
+    contactno varchar(20),
+    student_id integer NOT NULL PRIMARY KEY,
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
 );
 CREATE TABLE if not exists Teachers
 (
