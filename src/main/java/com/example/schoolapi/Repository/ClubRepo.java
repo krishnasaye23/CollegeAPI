89296fb04a2dbaa0df2710a6fbd5a459530a7bf5
Krishna A S <krishnaas@192.168.0.2>
2021-06-15 11:21:02 +0530

    https://github.com/krishnasaye23/CollegeAPI/issues/5#issue-910218269

--- src/main/java/com/example/schoolapi/Repository/ClubRepo.java
+++ src/main/java/com/example/schoolapi/Repository/ClubRepo.java
@@ -3,8 +3,8 @@ package com.example.schoolapi.Repository;
 import com.example.schoolapi.Entity.ClubEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
+
 @Repository
 public interface ClubRepo extends JpaRepository<ClubEntity,Integer> {
     List<ClubEntity> findAll();
