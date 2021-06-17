59fdfe531fc1a01b95b7a32b7ce5a0bef254f022
krishnasaye23 <krishna@banfico.com>
2021-06-17 12:04:05 +0530

    Added Spring Boot Servlet Initializer

--- src/main/java/com/example/schoolapi/SchoolapiApplication.java
+++ src/main/java/com/example/schoolapi/SchoolapiApplication.java
@@ -2,12 +2,10 @@ package com.example.schoolapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
-@SpringBootApplication//(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SchoolapiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
