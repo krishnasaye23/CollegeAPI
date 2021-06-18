1c34b7d39af2168c82253728cb42b8e42c3287a7
krishnasaye23 <krishna@banfico.com>
2021-06-18 18:54:23 +0530

    https://github.com/krishnasaye23/CollegeAPI/issues/2#issue-910204817

--- src/main/java/com/example/schoolapi/Model/Dept_Associations.java
+++ src/main/java/com/example/schoolapi/Model/Dept_Associations.java
@@ -1,5 +1,7 @@
 package com.example.schoolapi.Model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.persistence.Id;
@@ -7,7 +9,9 @@ import javax.persistence.MappedSuperclass;
 
 @Data
 @MappedSuperclass
+@ApiModel(description = "Details of the Department Associations")
 public class Dept_Associations {
     @Id
+    @ApiModelProperty(notes = "Id of the department association",required = true)
     private int asc_id;
 }
